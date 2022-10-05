5eee2bc4e160e82cf7c6aa7d939267fd70186063
James <72478345+wikssjs@users.noreply.github.com>
2022-10-05 13:42:02 -0400

    changed some color and added anumation

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const nav = document.getElementById('navigation');
+window.addEventListener('scroll',()=>{
+   if(window.scrollY >=30){
+    console.log(window.scrollY);
+    nav.classList.add('nav-scroll');
+   }else{
+    nav.classList.remove('nav-scroll');
+   }
+})
+
+
+let myDiv = document.getElementById("follow-cursor")
+function isTouchDevise(){
+    try{
+        document.createEvent("TouchEvent");
+        return true;
+    }catch(e){
+        return false;
+    }
+}
+
+
+const move = (e)=>{
+    try{
+        var x = !isTouchDevise() ? e.pageX : e.touches[0].pageX;
+        var y = !isTouchDevise() ? e.pageY : e.touches[0].pageY;
+    }catch(e){}
+    myDiv.style.left = x-30+"px";
+    myDiv.style.top = y-30+"px";
+}
+document.addEventListener('mousemove',(e)=>{
+    move(e);
+});
+document.addEventListener("touchmove",(e)=>{
+    move(e);
+})
